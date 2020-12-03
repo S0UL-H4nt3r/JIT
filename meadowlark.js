22dcc441ae92ad13b500288436e5ad01ee1aedb9
Soul-Hunter <mxmhuolq@zeroe.ml>
2020-12-03 21:51:56 +0200

    add qa tests

--- meadowlark.js
+++ meadowlark.js
@@ -8,9 +8,12 @@ app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
 app.set('port', process.env.PORT || 3000);
+app.use(function(req, res, next) {
+    res.locals.showTests = app.get('env') !== 'production' && res.query.test === '1';
+    next();
+});
 
 app.use(express.static(__dirname + '/public'));
-
 app.get('/', function(req, res) {
     res.render('home');
 });
